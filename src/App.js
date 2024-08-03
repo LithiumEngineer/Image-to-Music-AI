42df4be9fe789167ce430db71f11550794fa298a
Sarah Wang <sarah.w2258@yahoo.ca>
2024-08-03 13:08:58 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
-  return <div className="text-red-500">hi</div>
+
+  return <div className="text-red-500"> <button>Submit </button> <br></br>
+  </div>
 }
 
 export default App
