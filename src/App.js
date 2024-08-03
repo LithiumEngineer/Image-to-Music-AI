46ea4edb5b92979433e3489ffe2ec431e876701f
JackChujun <1yanjac@gmail.com>
2024-08-03 11:37:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg"
 import "./App.css"
 
 function App() {
-  return <div className="text-blue-500">hi</div>
+  return <div className="text-blue-500">hi hi</div>
 }
 
 export default App
