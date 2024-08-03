3f5b8030309fab3224ea1d430c34e48b482f9f0e
JackChujun <1yanjac@gmail.com>
2024-08-03 13:25:07 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import logo from "./logo.svg"
 import "./App.css"
 import React, { useState } from 'react';
+import Hi from "./components/textToMusic"
 
 function App() {
-  const [inputValue, setInputValue] = useState('');
-  const [output, setOutput] = useState('');
-  const handleInputChange = (event) => {
-    setInputValue(event.target.value);
-  };
-
-  
-  return <div 
-  className="text-red-500">
-    <h1>Input Lyrics</h1>
-    <div className="text-black-500">
-    <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        placeholder="Enter your lyrics"
-      /> <br></br>
-    </div>
-    <button>Submit </button> <br></br>
-  </div>
+  return <Hi/>
 }
 
 export default App
