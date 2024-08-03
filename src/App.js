d867c318afbddda992e40d90fb23b9b6dcf0b18e
Sarah Wang <sarah.w2258@yahoo.ca>
2024-08-03 13:18:37 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import logo from "./logo.svg"
 import "./App.css"
+import React, { useState } from 'react';
 
 function App() {
+  const [inputValue, setInputValue] = useState('');
+  const [output, setOutput] = useState('');
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-  return <div className="text-red-500"> <button>Submit </button> <br></br>
+  
+  return <div 
+  className="text-red-500">
+    <h1>Input Lyrics</h1>
+    <div className="text-black-500">
+    <input
+        type="text"
+        value={inputValue}
+        onChange={handleInputChange}
+        placeholder="Enter your lyrics"
+      /> <br></br>
+    </div>
+    <button>Submit </button> <br></br>
   </div>
 }
 
