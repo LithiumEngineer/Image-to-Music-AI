0444205800f3af22272c6aa5394df8f7a4d6c58c
LithiumEngineer <haochenkang@gmail.com>
2024-08-04 01:24:49 -0400

    added loader screen

--- src/components/resultspage/index.jsx
+++ src/components/resultspage/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import { useLocation, useNavigate } from "react-router-dom"
 import axios from "axios"
+import { Loading } from "react-loading-dot"
 
 const Results = () => {
   const location = useLocation()
@@ -22,8 +23,7 @@ const Results = () => {
       )
       const audioUrl = URL.createObjectURL(audioBlob)
       setAudioUrl(audioUrl)
-    } 
-    else {
+    } else {
       console.error("No audio data found in local storage.")
     }
   }
@@ -57,32 +57,48 @@ const Results = () => {
     }
   }, [inputList])
 
-  if (loading) return <div>Loading...</div>
+  if (loading)
+    return (
+      <div className="flex justify-around items-center w-screen h-screen bg-[#E1DBD5]">
+        <div className="absolute top-4 left-4" onClick={handleClick}>
+          <img
+            src="Main_Logo.jpg"
+            className="w-[75px] h-[75px] rounded-3xl ml-5 mt-3 hover:shadow-lg"
+          ></img>
+        </div>
+        <div className="mx-auto my-auto w-fit h-fit">
+          <p className="text-8xl text-center">...</p>
+          <p className="text-6xl font-bold text-center">Loading</p>
+          <p className="mt-2 text-lg text-center">
+            Audiolux.AI Is Generating Your Personalized Music
+          </p>
+        </div>
+      </div>
+    )
   if (error) return <div>Error: {error.message}</div>
 
   return (
     <div>
       <div className="w-screen h-screen" style={{ backgroundColor: "#DBBB9E" }}>
-        <div className="pt-3" onClick={handleClick}       >
-                <img
-          src="Main_Logo.jpg"
-          className="w-[75px] h-[75px] rounded-3xl ml-5 mt-3 hover:shadow-lg"
-        ></img>
+        <div className="pt-3" onClick={handleClick}>
+          <img
+            src="Main_Logo.jpg"
+            className="w-[75px] h-[75px] rounded-3xl ml-5 mt-3 hover:shadow-lg"
+          ></img>
         </div>
         <div className="flex items-center justify-center h-screen">
-        <div
-          className="w-2/3 h-2/3 flex flex-col rounded-3xl items-center justify-center"
-          style={{ backgroundColor: "#E5DDD0" }}
-        >
-
-        {audioUrl && (
-          <audio controls>
-            <source src={audioUrl} type="audio/wav" />
-            Your browser does not support the audio element.
-          </audio>
-        )}
+          <div
+            className="w-2/3 h-2/3 flex flex-col rounded-3xl items-center justify-center"
+            style={{ backgroundColor: "#E5DDD0" }}
+          >
+            {audioUrl && (
+              <audio controls>
+                <source src={audioUrl} type="audio/wav" />
+                Your browser does not support the audio element.
+              </audio>
+            )}
+          </div>
         </div>
-              </div>
       </div>
     </div>
   )
