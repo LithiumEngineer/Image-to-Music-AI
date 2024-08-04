5401432ead82ceff8ed93abf52665e6e8c052a47
LithiumEngineer <haochenkang@gmail.com>
2024-08-03 22:49:46 -0400

    added image upload and image recognition logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from "./logo.svg"
 import "./App.css"
-import React, { useState } from 'react';
+import React, { useState } from "react"
 import Hi from "./components/textToMusic"
+import ImageParser from "./components/imageParser"
 
 function App() {
-  return <Hi/>
+  return (
+    <>
+      <img src="react.png" className="h-20 w-20" id="reactimg"></img>
+      <ImageParser />
+    </>
+  )
 }
 
 export default App
